292a70ad57bfa8b06e88328da5b36c4afa5ad1f8
Nathan Hartman <nathanial.hartman@gmail.com>
2016-05-01 00:21:50 -0700

    added basic scaling

--- src/PanZoomElement.jsx
+++ src/PanZoomElement.jsx
@@ -7,13 +7,23 @@ export default class PanZoomElement extends Component {
     super(...arguments);
     this._lastX = 0;
     this._lastY = 0;
+
+    this.state = {
+      scale: 1,
+      translate: {x: 0, y: 0}
+    };
   }
 
   render(){
+    const style = {
+      transform: `translate(${this.state.translate.x}px, ${this.state.translate.y}px) scale(${this.state.scale})`
+    };
     return (
       <div className="pan-zoom-element"
-           onMouseDown={this._onMouseDown}>
-        <div ref="content" className="content-container noselect">
+           onMouseDown={this._onMouseDown}
+           onWheel={this._onWheel}>
+        <div ref="content" className="content-container noselect"
+             style={style}>
           {this.props.children}
         </div>
       </div>
@@ -30,14 +40,29 @@ export default class PanZoomElement extends Component {
   _onMouseUp(){
     document.removeEventListener('mouseup', this._onMouseUp, true);
     document.removeEventListener('mousemove', this._onMouseMove, true);
-    this._lastX = this._currentX;
-    this._lastY = this._currentY;
+    this._lastX = this.state.translate.x;
+    this._lastY = this.state.translate.y;
   }
 
   _onMouseMove(event){
-    this._currentX = (event.pageX - this._startX) + this._lastX;
-    this._currentY = (event.pageY - this._startY) + this._lastY;
-    this.refs.content.style.transform = `translate(${this._currentX}px, ${this._currentY}px)`;
+    this.setState({
+      translate: {
+        x: (event.pageX - this._startX) + this._lastX,
+        y: (event.pageY - this._startY) + this._lastY
+      }
+    });
+  }
+
+  _onWheel(event){
+    if(event.deltaY < 0){
+      this.setState({
+        scale: this.state.scale * 1.1
+      });
+    } else {
+      this.setState({
+        scale: this.state.scale * 0.9
+      });
+    }
   }
 
 }
